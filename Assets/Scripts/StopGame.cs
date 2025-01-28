6fcac9c1b728834938ca25cb1ece83ab0a98d5d0
miguelturmo <125253347+miguelturmo@users.noreply.github.com>
2025-01-29 00:29:38 +0100

    .

--- Assets/Scripts/StopGame.cs
+++ Assets/Scripts/StopGame.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.UI;
 public class StopGame : MonoBehaviour
 {
+    public TMP_Dropdown resolucionesDropDown;
+    Resolution[] resoluciones;
+
+    private void Start()
+    {
+
+        RevisarResolucion();
+    }
     public void PauseWorld()
     {
         Time.timeScale = 0.0f;
@@ -12,5 +21,42 @@ public class StopGame : MonoBehaviour
     public void ReturnTimeWorld()
     {
         Time.timeScale = 1.0f;
+    }
+    public void SetFullScreen(bool isfullscreen)
+    {
+        Screen.fullScreen = isfullscreen;
+    }
+
+    public void RevisarResolucion()
+    {
+        resoluciones = Screen.resolutions;
+        resolucionesDropDown.ClearOptions();
+        List<string> opciones = new List<string>();
+        int resolucionActual = 0;
+
+        for(int i = 0; i< resoluciones.Length; i++)
+        {
+            string opcion = resoluciones[i].width + " x " + resoluciones[i].height;
+            opciones.Add(opcion);
+
+            if(Screen.fullScreen && resoluciones[i].width == Screen.currentResolution.width && resoluciones[i].height == Screen.currentResolution.height)
+            {
+                resolucionActual = i;
+            }
+        }
+
+        resolucionesDropDown.AddOptions(opciones);
+        resolucionesDropDown.value = resolucionActual;
+        resolucionesDropDown.RefreshShownValue();
+
+        resolucionesDropDown.value = PlayerPrefs.GetInt("numeroResolucion", 0);
+    }
+
+    public void CambiarResolucion(int indiceResolucion)
+    {
+        PlayerPrefs.SetInt("numeroResolucion", resolucionesDropDown.value);
+
+        Resolution resolucion = resoluciones[indiceResolucion];
+        Screen.SetResolution(resolucion.width, resolucion.height, Screen.fullScreen);
     }
 }
