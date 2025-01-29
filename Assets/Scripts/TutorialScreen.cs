1f360f6c71767862ce54357764dfab8605487286
miguelturmo <125253347+miguelturmo@users.noreply.github.com>
2025-01-29 18:38:07 +0100

    tutorial terminado

--- Assets/Scripts/TutorialScreen.cs
+++ Assets/Scripts/TutorialScreen.cs
@@ -8,19 +8,34 @@ public class TutorialScreen : MonoBehaviour
     public Canvas playerUI;
     public Canvas Controlls;
     public Canvas EvolveManager;
-   
-   public void BacktoMenu()
-    {
-        
-    }
-    public void playerUIToControlls()
-    {
-        playerUI.gameObject.SetActive(false);
-        Controlls.gameObject.SetActive(true);
-    }
-    public void ControllsToPlayerUI()
+    public Canvas Ambient;
+   public void Start()
     {
         playerUI.gameObject.SetActive(true);
+        Ambient.gameObject.SetActive(false);
+        EvolveManager.gameObject.SetActive(false);
+        Controlls.gameObject.SetActive(false);
+    }
+    public void playerUIToAmbient()
+    {
+        playerUI.gameObject.SetActive(false);
+        Ambient.gameObject.SetActive(true);
+    }
+    
+    public void AmbientToPlayerUI()
+    {
+        playerUI.gameObject.SetActive(true);
+        Ambient.gameObject.SetActive(false);
+    }
+    public void AmbientToControlls()
+    {
+        Ambient.gameObject.SetActive(false);
+        Controlls.gameObject.SetActive(true);
+    }
+    
+    public void ControllsToAmbient()
+    {
+        Ambient.gameObject.SetActive(true);
         Controlls.gameObject.SetActive(false);
     }
     public void ControllsToEvolveManager()
