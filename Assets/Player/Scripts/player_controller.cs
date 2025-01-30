8d4d8c782eaa5e86b0f6b94c639a5ac92de5b1f0
AndreuMiroSabate <andreu.miro@estudiantat.upc.edu>
2025-01-30 12:48:24 +0100

    last fix

--- Assets/Player/Scripts/player_controller.cs
+++ Assets/Player/Scripts/player_controller.cs
@@ -309,6 +309,13 @@ public class player : MonoBehaviour
             damageTimer = 0f;
         }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("InstantKill"))
+        {
+            actuallife = 0;
+        }
+    }
 
     private void TriggerDamage()
     {
